ea9174b55c78eeaeba8e1f108415ce05b88a1cb2
jordantrh <123779930+jordantrh@users.noreply.github.com>
2023-02-17 12:57:25 -0500

    Add files via upload
    working quick code

--- quick.cpp
+++ quick.cpp
@@ -1,64 +1,142 @@
 // quick.cpp
+// #include "list.cpp"
 
 #include "volsort.h"
 
 #include <iostream>
 
+// using namespace std;
 // Prototypes
 
 Node *qsort(Node *head, bool numeric);
 void  partition(Node *head, Node *pivot, Node *&left, Node *&right, bool numeric);
 Node *concatenate(Node *left, Node *right);
-
+int i = 0;
 // Implementations
 
 void quick_sort(List &l, bool numeric) {
-    Node *thehead = qsort(l.head, numeric);
+    l.head = qsort(l.head, numeric);
 }
 
 Node *qsort(Node *head, bool numeric) {
     Node *Left, *Right, *Pivot;
-    
+    // Node *Left_check, *Right_check;
+    Left = NULL;
+    Right = NULL;
     //base case
     //if one node or empty list
-    if (head->next == NULL) {
+    if (head->next == NULL || head == NULL) {
         return head;
     }
-
     
     //divide
-    //pivot = first node
+    //Pivot = first node
     Pivot = head;
-    
     
     //partition()
     partition(head, Pivot, Left, Right, numeric);
-    
+    //cout << "did partition";
     //recursivelly call qsort() on left
-    Left = qsort(Left, numeric);
-    //recursively call qsort() on right
-    Right = qsort(Right, numeric);
+    // Left_check = Left;
+    // Right_check = Right;
 
-    return concatenate(Left, Right);
+    //  if(Left_check != NULL){
+    //     cout << "Left: ";
+    //     while(Left_check->next != NULL){
+    //         cout << Left_check->number << "|" ;
+    //         Left_check = Left_check->next;
+    //     }
+    //     cout << Left_check->number << "|" ;
+    //     cout << endl;
+    // }
+    // if(Right_check != NULL){
+    //     cout << "Right: ";
+    //     while(Right_check->next != NULL){
+    //         cout << Right_check->number << "|" ;
+    //         Right_check = Right_check->next;
+    //     }
+    //     cout << Right_check->number << "|" ;
+    //     cout << endl;
+    // }
+    
+    if(Left != NULL){
+        Left = qsort(Left, numeric);
+    }
+    
+    //recursively call qsort() on right
+    if(Right != NULL){
+        Right = qsort(Right, numeric);
+    }
+    
+    // if(Left != NULL){
+    //     while(Left->next != NULL){
+    //         cout << "Left number b4 concat: " << Left->number << "|" ;
+    //         Left = Left->next;
+    //     }
+    //     cout << Left->number << "|" ;
+    //     cout << endl;
+    // }
+    //free(Left);
+    //free(Right);
+    //head->next = NULL;
+    head->next = NULL;
+    head = concatenate(Left, head);
+    return concatenate(head, Right);
 }
 
 void partition(Node *head, Node *pivot, Node *&left, Node *&right, bool numeric) {
 
-    
+    Node *Tail_L, *Tail_R, *iterator;
     //divides the list into two smaller sublists. everything smaller than or equal to the pivot goes to the left. 
     //Anything bigger goes to the right
-    while(head->next != NULL){
-        head = head->next;
-        if(node_number_compare(head, pivot)){
-            right = head;
-            right = right->next;
+    Tail_L = NULL;
+    Tail_R = NULL;
+    // cout << "pivot in partition " << i << ": " << pivot->number << endl;
+    //cout << "partition ";
+    iterator = head->next; 
+    while(iterator != NULL){
+       //
+       //cout << "this is compare in partition " << i << ": " << iterator->number << endl;
+        if(node_number_compare(pivot, iterator)){
+            if(right == NULL){
+                //cout << "Compare greater than pivot " << i << endl << endl;
+                right = iterator;
+                Tail_R = iterator; 
+                //right = Tail_R;
+            }else{
+                //cout << "Compare greater than pivot " << i << endl;
+                Tail_R->next = iterator;
+                Tail_R = Tail_R->next;
+                //cout << "Tail_R is :" << Tail_R->number << endl << endl;
+            }
         }else{
-            left = head;
-            left = left->next; 
+          if(left == NULL){
+                //cout << "Compare less than pivot " << i << endl << endl;
+                left = iterator;
+                Tail_L = iterator; 
+                //left = Tail_L;
+            }else{
+                //cout << "Compare less than pivot " << i << endl;
+                Tail_L->next = iterator;
+                Tail_L = Tail_L->next;
+                //cout << "Tail_L is :" << Tail_L->number << endl << endl;
+            }
         }
+
+        
+        iterator = iterator->next;
     }
-    right->next = NULL;
-    left->next = NULL;
+
+    if (Tail_R != NULL) Tail_R->next = NULL;
+    if (Tail_L != NULL) Tail_L->next = NULL;
+
+    i++;
+    //free(iterator);
+    //IF LEFT = NULL
+    //LEFT = HEAD
+    //LEFT_TAIL = HEAD;
+    //Left_Tail = Left_Tail-> Next; 
+    
 }
 
 Node *concatenate(Node *left, Node *right) {
@@ -66,24 +144,72 @@ Node *concatenate(Node *left, Node *right) {
     //body is the rest of the list
     Node *Head, *Body;
     //combines two sublist.
-    Head = left;
-    Head->next = Body;
+    //Head = left;
     
-    while(left->next != NULL){
-        left = left->next;
-        Body = left;
-        Body = Body->next;
+    if(left == NULL){
+        Head = right;
+        return Head;
+        //cout << "right value " << i << ":" << right->number << endl;
+       // Body = left->next;
+       // Head->next = Body;
+    }
+    else{
+        Head = left;
+    }
+    
+    Body = left;
+    if(Body != NULL){
+        //cout << " i: " << i << endl;
+        //cout << "Left value " << i << ":" << left->number << endl;
+        while(Body->next != NULL){
+           Body = Body->next;
+           //cout << left->number << " " ;
+        //    Body = left;
+        //    Body = Body->next;
+           
+        }
+        //cout << endl;
     }
 
-    Body = right;
-    Body = Body->next;
-    while(right->next != NULL){
-        right = right->next;
-        Body = right;
-        Body = Body->next;
-    }
+    Body->next = right;
+    //left->next = right;
+    //Head->next = Body;
+    //i++;
+
+    // if(Head != NULL){
+    //     while(Head->next != NULL){
+    //         cout << "Concatenated: " << Head->number << "|" ;
+    //         Head = Head->next;
+    //     }
+    //     cout << endl;
+    // }
 
     return Head;
 
 }
 
+// int main() {
+//     bool numeric = true;
+//     List l;
+    
+//     l.push_front("2");
+//     l.push_front("22");
+//     l.push_front("3");
+//     l.push_front("45");
+//     l.push_front("4");
+//     l.push_front("4");
+//     Node* temp = l.head;
+    
+//     for(int i = 0; i < l.size; i++){
+//       cout << "woo " << temp->number << endl;
+//        temp = temp->next;
+//     }
+
+//     quick_sort(l, numeric);
+//     temp = l.head; 
+//     cout << l.size << "sorteeeeeed \n\n";
+//     for(int i = 0; i < l.size; i++){
+//       cout << "woo " << temp->number << endl;
+//         temp = temp->next;
+//     }
+// }
