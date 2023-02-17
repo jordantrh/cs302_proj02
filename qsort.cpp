ddc3e238c8afce497ff1da0d1ae3d538b178e0ae
atran8utk <92347696+atran8utk@users.noreply.github.com>
2023-02-17 16:28:48 -0500

    Comments added

--- qsort.cpp
+++ qsort.cpp
@@ -11,6 +11,7 @@
 
 using namespace std;
 
+//Compare function for numbers
 int node_number_compare(const void * a, const void * b){
     int aVal = (*(const Node **)a)->number;
     int bVal = (*(const Node **)b)->number;
@@ -18,16 +19,21 @@ int node_number_compare(const void * a, const void * b){
     return aVal - bVal;
 }
 
+
+//Compare function for strings
 int node_string_compare(const void * a, const void * b)
 {
     return ((*(const Node **)a)->string).compare((*(const Node **)b)->string);
 }
 
+//Qsort function
 void qsort_sort(List &l, bool numeric) {
   const int listSize = l.size;
   
+    // set vector for sorting
   vector<Node *> nodeVect;
   
+    //Place nodes in vector
   Node* tempN = l.head;
   while(tempN->next != NULL){
       Node* nextN = tempN->next;
@@ -36,6 +42,7 @@ void qsort_sort(List &l, bool numeric) {
   }
   nodeVect.push_back(tempN);
   
+    //Sort vector based on if numeric or not
   if(numeric){
       std::qsort(&nodeVect[0], listSize, sizeof(Node *), node_number_compare);
   }
@@ -43,6 +50,7 @@ void qsort_sort(List &l, bool numeric) {
       std::qsort(&nodeVect[0], listSize, sizeof(Node *), node_string_compare);
   }
   
+    //Place nodes back in vector after sorting
   l.head = nodeVect[0];
   tempN = l.head;
   for (unsigned int i = 1; i < nodeVect.size(); i++) {
