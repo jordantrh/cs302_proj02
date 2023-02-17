76f8496ec1c8f165c42913462e8176d4daffebbe
Mwebb512 <120092335+Mwebb512@users.noreply.github.com>
2023-02-17 17:32:20 -0500

    Add files via upload

--- quick.cpp
+++ quick.cpp
@@ -26,27 +26,15 @@ Node *qsort(Node *head, bool numeric) {
         return head;
     }
     Head_check = head;
-    /*
-    if(Head_check != NULL){
-        while(Head_check->next != NULL){
-            cout << Head_check->number << "|" ;
-            Head_check = Head_check->next;
-        }
-        cout << Head_check->number << "|" ;
-        cout << endl;
-    }
-    */
+    
     //divide
     //Pivot = first node
     Pivot = head;
     
-    
     //partition()
     partition(head, Pivot, Left, Right, numeric);
-    //recursivelly call qsort() on left
-    
 
-    
+    //recursivelly call qsort() on left
     if(Left != NULL){
         Left = qsort(Left, numeric);
     }
@@ -55,7 +43,7 @@ Node *qsort(Node *head, bool numeric) {
     if(Right != NULL){
         Right = qsort(Right, numeric);
     }
-    
+    //combine the two list
     head->next = NULL;
     head = concatenate(Left, head);
     return concatenate(head, Right);
@@ -69,42 +57,28 @@ void partition(Node *head, Node *pivot, Node *&left, Node *&right, bool numeric)
     Tail_L = NULL;
     Tail_R = NULL;
     bool condition;
-  
-    
-    //cout << "this is pivot in partition " << i << ": " << pivot->number << endl;
-    //cout << "partition ";
+    //use iterator to cycle through the list
     iterator = head->next; 
     while(iterator != NULL){
-       //
+       //for if statement
         value_is_numeric = (numeric) ? node_number_compare(pivot, iterator) : node_string_compare(pivot, iterator);
-
-        //cout << "this is iterator in partition " << i << ": " << iterator->number << endl;
+        //if greater, go to right list
         if(value_is_numeric){
             if(right == NULL){
-                //cout << "Compare greater than pivot " << i << endl << endl;
                 right = iterator;
                 Tail_R = iterator; 
-                //Tail_R->next = 0;
-                //right = Tail_R;
-                //Tail_R = Tail_R->next;
-                //cout << "Tail_R is :" << Tail_R->number << endl;
             }else{
-                //cout << "Compare greater than pivot " << i << endl;
                 Tail_R->next = iterator;
                 Tail_R = Tail_R->next;
-               // cout << "Tail_R is :" << Tail_R->number << endl;
             }
+        //if less than, go to right list
         }else{
             if(left == NULL){
-                //cout << "Compare less than pivot " << i << endl << endl;
                 left = iterator;
                 Tail_L = iterator; 
-                //left = Tail_L;
             }else{
-                //cout << "Compare less than pivot " << i << endl;
                 Tail_L->next = iterator;
                 Tail_L = Tail_L->next;
-                //cout << "Tail_L is :" << Tail_L->number << endl << endl;
             }
         }
         
@@ -120,14 +94,6 @@ void partition(Node *head, Node *pivot, Node *&left, Node *&right, bool numeric)
         Tail_R->next = NULL;
     }
     
-    //i++;
-    //Tail_L->next = NULL;
-    //free(iterator);
-    //IF LEFT = NULL
-    //LEFT = HEAD
-    //LEFT_TAIL = HEAD;
-    //Left_Tail = Left_Tail-> Next; 
-    
 }
 
 Node *concatenate(Node *left, Node *right) {
@@ -135,33 +101,24 @@ Node *concatenate(Node *left, Node *right) {
     //body is the rest of the list
     Node *Head, *Body;
     //combines two sublist.
-    //Head = left;
     
+    
+    //if left is null head just needs to be set to right
     if(left == NULL){
         Head = right;
         return Head;
-        //return right;
-       // Body = left->next;
-       // Head->next = Body;
     }else{
         Head = left;
     }
     
+    //use body to get to end of left and set the ->next of the last value to right
     Body = left;
     if(Body != NULL){
-        //cout << " i: " << i << endl;
-        //cout << "Left value " << i << ":" << left->number << endl;
         while(Body->next != NULL){
            Body = Body->next;
-
-           
         }
-        //cout << endl;
     }
     Body->next = right;
-    //Head->next = Body;
-    //i++;
-
     
     return Head;
 
